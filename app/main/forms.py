46fe986574f9937c23d7e796096477716dba5250
bjjvvv <bjjvvv@gmail.com>
2015-10-23 15:51:56 +0800

    完成消息发福，在主页面和用户页面显示消息

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,6 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField, TextField, BooleanField, \
-    SelectField
+    SelectField, TextAreaField
 from wtforms.validators import Length, Required, Email, Regexp
 from wtforms import ValidationError
 from ..models import User, Role
@@ -43,3 +43,7 @@ class EditProfileAdminForm(Form):
         if field.data != self.user.username and \
                 User.query.filter_by(username=field.data).first():
             raise ValidationError('Username already in use')
+
+class PostForm(Form):
+    body = TextAreaField("What's on your mind", validators=[Required()])
+    submit = SubmitField('Submit')
